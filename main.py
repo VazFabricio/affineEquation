e3f8bfd5b7b0c39e4d13286508777f96f2139bde
fabricio vaz <fabricio.vaz2004@gmail.com>
2023-03-15 09:12:29 -0300

    Added the option to change the points coordinates easily. Instead of changing it in the operation, you change it in the variables for points.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import sympy
 x, y = sympy.symbols('x y')
 
+
+#INPUT
 #Value for X, to calculate the Y value in function of X
 in_x = 5
 
+#INPUT
 #Insert the coordinates in the second and third row in the first and second element of each
-A = sympy.Matrix([[x, y, 1], [-1, 3, 1], [2, 7, 1]])
+point1 = (-1, 3)
+point2 = (2, 7)
+
+#calculation
+A = sympy.Matrix([[x, y, 1], [point1[0], point1[1], 1], [point2[0], point2[1], 1]])
 
 det_A = A.det()
-print("Determinant: ", det_A)
+print("Determinant =", det_A)
 
 y_expr = sympy.solve(det_A, y)[0]
 print("y =", y_expr)
